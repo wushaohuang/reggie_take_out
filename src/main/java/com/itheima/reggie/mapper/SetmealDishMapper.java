c8d8267dbc4db52f755d630d86e4f535d0edc849
ShaoHuang WU <shaohuang.wu1@se.com>
2023-04-06 20:36:53 +0800

    添加 新增套餐功能

---
+++ src/main/java/com/itheima/reggie/mapper/SetmealDishMapper.java
@@ -0,0 +1,8 @@
+package com.itheima.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.entity.SetmealDish;
+
+public interface SetmealDishMapper extends BaseMapper<SetmealDish> {
+}
